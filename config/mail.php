42097abcb5e5ae0e3125743ca784c6e00239c287
Tülin Eren <erentulin5@gmail.com>
2025-12-05 02:28:28 +0300

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -2,7 +2,8 @@
 return [
     'host' => 'smtp.gmail.com',
     'username' => 'iletisimformu02@gmail.com',
-    'password' => 'sxgphknwdoyjwkwm',
+    'password' => '',
     'port' => 587,
     'secure' => 'tls'
 ];
+
